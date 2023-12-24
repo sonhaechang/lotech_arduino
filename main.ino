a36689862a52d7f56f316f13d6a06105595e049f
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-12-24 16:07:12 +0900

    refector: Modify main.ino and README.md
    1. main.ino
       - 추후 와이파이 모듈로 업데이하기 위한 확장성 작업
       - 특정 디바이스에서 작업중일때 다른 디바이스에서 들어오는 작업 방지를 위한 코드 수정
    2. README.md 수정

--- main.ino
+++ main.ino
@@ -1,4 +1,7 @@
-#define MAX_VALVE_CNT 8
+const int MAX_VALVE_CNT = 8;
+const int MAX_RECEIVED_LEN = 14;
+const char START_COMMAND = "s";
+const char END_COMMAND = "e";
 unsigned char VALVES[MAX_VALVE_CNT] = {1, 2, 3, 4, 5, 6, 7, 8};
 
 void setup() {
@@ -11,14 +14,41 @@ void setup() {
 }
 
 void loop() {
-  	while (Serial.available() > 0) {
-		char command = Serial.read();
-		String cmd = String(command);
+	static String receivedString = ""; 
+	static String activePK = "";
 
-		for (int i=0; i<MAX_VALVE_CNT; i++) {
-			if (VALVES[i] == cmd.toInt()) {
-				digitalWrite(VALVES[i], !digitalRead(VALVES[i]));
+  	while (Serial.available() > 0) {
+        char incomingChar = Serial.read();
+        
+        if (incomingChar == "\n") {
+            receivedString = "";
+        } else {
+            receivedString += incomingChar;
+        }
+
+		if (receivedString.length() == MAX_RECEIVED_LEN) {
+			int index_1 = receivedString.indexOf(":");
+			int index_2 = receivedString.length();
+			String pk = receivedString.substring(0, index_1);
+			String cmd = receivedString.substring(index_1 + 1, index_2);
+
+			if (cmd == START_COMMAND && activePK == "") {
+				activePK = pk;
+			} else if (cmd == END_COMMAND) {
+				activePK = "";
 			}
-    	}
-	}
+
+			if (activePK == pk) {
+				Serial.print("1");
+
+				for (int i=0; i<MAX_VALVE_CNT; i++) {
+					if (VALVES[i] == cmd.toInt()) {
+						digitalWrite(VALVES[i], !digitalRead(VALVES[i]));
+					}
+				}
+			} else {
+				Serial.print("0");
+			}
+		}
+    }
 }
